e3278cc6a29760a88c0c855b528e04774f28431e
wobu <buchnerw@googlemail.com>
2018-11-06 08:43:01 +0100

    AD ex5 task 1 (search algorithm)

---
+++ AD/ex5/task1/Algorithm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using SortAlgorithm;
+
+namespace task1
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            for (int k = 1; k <= 10; k++)
+            {
+                var a = generateRandomList(10);
+                var s = new Random().Next(-14, 14);
+
+                // n log n
+                new HeapSort().sort(a);
+
+                var i = 0;
+                var j = a.Count - 9;
+
+                while (a[i] + a[j] != s && i < j)
+                {
+                    var tmp = a[i] + a[j];
+
+                    if (tmp > s)
+                    {
+                        j--;
+                    }
+                    else if (tmp < s)
+                    {
+                        i++;
+                    }
+                }
+
+                Console.Write($"[{String.Join(',', a)}]\ts: {s}\t");
+
+                if (a[i] + a[j] == s)
+                {
+                    Console.WriteLine($"True => {a[i]} + {a[j]}");
+                }
+                else
+                { 
+                    Console.WriteLine("False");
+                }
+                // TODO implement algorithm
+            }
+        }
+
+        static List<int> generateRandomList(int n)
+        {
+            var list = new List<int>(n);
+            var random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(random.Next(-10, 10));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ AD/ex5/task1/HeapSort.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+
+namespace SortAlgorithm
+{
+    interface SortAlgorithm
+    {
+        void sort(List<int> list);
+    }
+
+    class HeapSort : SortAlgorithm
+    {
+        public Action<List<int>> IterationDelegate
+        {
+            get; set;
+        } = (list) => { };
+
+        public void sort(List<int> list)
+        {
+            buildHeap(list);
+
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                swap(list, 0, i);
+                IterationDelegate(list);
+                heapify(list, 0, i - 1, 0);
+            }
+        }
+
+        void buildHeap(List<int> list)
+        {
+            for (int i = (list.Count - 2) / 2; i >= 0; i--)
+            {
+                heapify(list, 0, list.Count - 1, i);
+            }
+        }
+
+        void heapify(List<int> list, int f, int l, int root)
+        {
+            int left = f + (root - f) * 2 + 1;
+            int right = f + (root - f) * 2 + 2;
+            int max = 0;
+
+            if (left <= l && list[left] > list[root])
+            {
+                max = left;
+            }
+            else
+            {
+                max = root;
+            }
+
+            if (right <= l && list[right] > list[max])
+            {
+                max = right;
+            }
+
+            if (max != root) 
+            {
+                swap(list, root, max);
+                heapify(list, f, l, max);
+            }
+        }
+
+
+        static void swap(List<int> list, int a, int b)
+        {
+            int tmp = list[a];
+            list[a] = list[b];
+            list[b] = tmp;
+        }
+    }
+}
